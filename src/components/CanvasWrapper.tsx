6cb285aa3b1cf66a0380611fc892a79e9824fbbe
nullsploit01 <harshaldharmik099@gmail.com>
2025-04-08 08:54:10 +0530

    add more entracne pillars

--- src/components/CanvasWrapper.tsx
+++ src/components/CanvasWrapper.tsx
@@ -83,13 +83,21 @@ const CanvasWrapper = () => {
           </mesh>
           // entrance
           <group>
-            <mesh>
-              <boxGeometry args={[1, 5, 1]} />
+            <mesh position={[0, 1.75, -0.75]}>
+              <boxGeometry args={[1, 3.5, 1.5]} />
               <meshNormalMaterial />
             </mesh>
-            <mesh position={[-3, 0, 0]}>
-              <boxGeometry args={[1, 7, 1]} />
+            <mesh position={[-3, 1.75, -0.75]}>
+              <boxGeometry args={[1, 3.5, 1.5]} />
               <meshNormalMaterial />
+            </mesh>
+            <mesh position={[-1.5, 3.3, -0.75]} rotation-y={Math.PI * 0.5}>
+              <boxGeometry args={[1.5, 0.4, 2]} />
+              <meshBasicMaterial color="red" />
+            </mesh>
+            <mesh position={[-1.5, 2.9, -0.75]} rotation-y={Math.PI * 0.5}>
+              <boxGeometry args={[1.5, 0.4, 2]} />
+              <meshBasicMaterial color="black" />
             </mesh>
           </group>
           // room
