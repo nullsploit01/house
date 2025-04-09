de6d22727ce102516028d74cf317107a4f88e959
nullsploit01 <harshaldharmik099@gmail.com>
2025-04-09 09:43:10 +0530

    add garage pathway

--- src/components/Floor.tsx
+++ src/components/Floor.tsx
@@ -40,22 +40,70 @@ const Floor = () => {
   floorDisplacementMap.wrapS = RepeatWrapping;
   floorDisplacementMap.wrapT = RepeatWrapping;
 
+  const garagePathColorTexture = useLoader(
+    TextureLoader,
+    'textures/floor/herringbone_brick_02_1k/herringbone_brick_02_diff_1k.png',
+  );
+
+  garagePathColorTexture.repeat.set(2, 2);
+  garagePathColorTexture.wrapS = RepeatWrapping;
+  garagePathColorTexture.wrapT = RepeatWrapping;
+  garagePathColorTexture.colorSpace = SRGBColorSpace;
+
+  const garagePathARMTexture = useLoader(
+    TextureLoader,
+    'textures/floor/herringbone_brick_02_1k/herringbone_brick_02_arm_1k.png',
+  );
+
+  garagePathARMTexture.wrapS = RepeatWrapping;
+  garagePathARMTexture.wrapT = RepeatWrapping;
+  garagePathARMTexture.colorSpace = SRGBColorSpace;
+
+  const garagePathNormalTexture = useLoader(
+    TextureLoader,
+    'textures/floor/herringbone_brick_02_1k/herringbone_brick_02_nor_gl_1k.png',
+  );
+
+  garagePathNormalTexture.wrapS = RepeatWrapping;
+  garagePathNormalTexture.wrapT = RepeatWrapping;
+  garagePathNormalTexture.colorSpace = SRGBColorSpace;
+
+  const garagePathDisplacementMap = useLoader(
+    TextureLoader,
+    'textures/floor/herringbone_brick_02_1k/herringbone_brick_02_disp_1k.png',
+  );
+
   return (
-    <mesh rotation-x={-Math.PI * 0.5}>
-      <planeGeometry args={[50, 50, 200, 200]} />
-      <meshStandardMaterial
-        map={floorColorTexture}
-        aoMap={floorARMTexture}
-        metalnessMap={floorARMTexture}
-        roughnessMap={floorARMTexture}
-        normalMap={floorNormalTexture}
-        displacementMap={floorDisplacementMap}
-        displacementScale={0.3}
-        displacementBias={-0.2}
-        alphaMap={floorAlphaTexture}
-        transparent
-      />
-    </mesh>
+    <group>
+      <mesh rotation-x={-Math.PI * 0.5}>
+        <planeGeometry args={[50, 50, 200, 200]} />
+        <meshStandardMaterial
+          map={floorColorTexture}
+          aoMap={floorARMTexture}
+          metalnessMap={floorARMTexture}
+          roughnessMap={floorARMTexture}
+          normalMap={floorNormalTexture}
+          displacementMap={floorDisplacementMap}
+          displacementScale={0.3}
+          displacementBias={-0.2}
+          alphaMap={floorAlphaTexture}
+          transparent
+        />
+      </mesh>
+      <mesh position={[-5, 0.001, 10]} rotation-x={-Math.PI * 0.5}>
+        <planeGeometry args={[5, 20]} />
+        <meshStandardMaterial
+          map={garagePathColorTexture}
+          aoMap={garagePathARMTexture}
+          metalnessMap={garagePathARMTexture}
+          roughnessMap={garagePathARMTexture}
+          normalMap={garagePathNormalTexture}
+          displacementMap={garagePathDisplacementMap}
+          displacementBias={-0.01}
+          displacementScale={0.1}
+        />
+      </mesh>
+    </group>
   );
 };
 
