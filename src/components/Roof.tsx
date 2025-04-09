716000805d272c2fa18f4a95292a389c6f7575e3
nullsploit01 <harshaldharmik099@gmail.com>
2025-04-09 08:35:40 +0530

    add roof textures

--- src/components/Roof.tsx
+++ src/components/Roof.tsx
@@ -1,5 +1,11 @@
-import { DoubleSide } from "three";
+import {
+  DoubleSide,
+  RepeatWrapping,
+  SRGBColorSpace,
+  TextureLoader,
+} from "three";
 import TriangleGeometry from "./geometry/Triangle";
+import { useLoader } from "@react-three/fiber";
 
 const Roof = ({
   width = 2, // width of base (X)
@@ -13,6 +19,43 @@ const Roof = ({
   color?: string;
   position?: [number, number, number];
 }) => {
+  const roofColorTexture = useLoader(
+    TextureLoader,
+    "textures/roof/ceramic_roof_01_1k/ceramic_roof_01_diff_1k.png"
+  );
+
+  roofColorTexture.colorSpace = SRGBColorSpace;
+  roofColorTexture.repeat.set(3, 1);
+  roofColorTexture.wrapS = RepeatWrapping;
+  roofColorTexture.wrapT = RepeatWrapping;
+
+  const roofARMTexture = useLoader(
+    TextureLoader,
+    "textures/roof/ceramic_roof_01_1k/ceramic_roof_01_arm_1k.png"
+  );
+
+  roofARMTexture.repeat.set(3, 1);
+  roofARMTexture.wrapS = RepeatWrapping;
+  roofARMTexture.wrapT = RepeatWrapping;
+
+  const roofNormalTexture = useLoader(
+    TextureLoader,
+    "textures/roof/ceramic_roof_01_1k/ceramic_roof_01_nor_gl_1k.png"
+  );
+
+  roofNormalTexture.repeat.set(3, 1);
+  roofNormalTexture.wrapS = RepeatWrapping;
+  roofNormalTexture.wrapT = RepeatWrapping;
+
+  const roofDisplacementTexture = useLoader(
+    TextureLoader,
+    "textures/roof/ceramic_roof_01_1k/ceramic_roof_01_disp_1k.png"
+  );
+
+  roofDisplacementTexture.repeat.set(3, 1);
+  roofDisplacementTexture.wrapS = RepeatWrapping;
+  roofDisplacementTexture.wrapT = RepeatWrapping;
+
   return (
     <TriangleGeometry
       height={height}
@@ -20,7 +63,14 @@ const Roof = ({
       depth={depth}
       position={position}
     >
-      <meshNormalMaterial side={DoubleSide} />
+      <meshStandardMaterial
+        side={DoubleSide}
+        map={roofColorTexture}
+        aoMap={roofARMTexture}
+        metalnessMap={roofARMTexture}
+        normalMap={roofNormalTexture}
+        roughnessMap={roofARMTexture}
+      />
     </TriangleGeometry>
   );
 };
